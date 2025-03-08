67d00a7379f0b31bf2b242a9ad72c1fa35762b02
TraFost <rahwisdilfiqrak@gmail.com>
2025-03-08 10:43:11 +0700

    chore(auth): remove commented-out code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,14 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  HttpCode,
-  // example how to protect route with jwt
-  // UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, HttpCode } from '@nestjs/common';
 
 import { LoginDto, LoginDtoSchema } from './dto/login.dto';
 import { RegisterDto, RegisterSchema } from './dto/register.dto';
 import { AuthService } from './auth.service';
 import { ZodValidationPipe } from '../common/pipes/zod-validation.pipe';
 
-// example how to protect route with jwt
-// import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
-
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  // example how to protect route with jwt
-  // @UseGuards(JwtAuthGuard)
   @Get()
   getHello() {
     return this.authService.getHello();
